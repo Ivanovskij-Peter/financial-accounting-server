14032320ae8d4ee0dc586d59391d2fa0b624404b
Sasha Skryd <sashaskryd@gmail.com>
2021-03-11 20:14:38 +0200

    testing

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,16 +1,17 @@
 async function updateBalance(req, res) {
-  const { balance } = req.body
-//   const validationBalance = Joi.object({
-//     balance: Joi.number().min(1),
-//   })
-    if (balance >= 1) {
-        try {
-          await User.findByIdAndUpdate(req.user._id, { ...req.body, balance: validationBalance }, { new: true })
-          res.status(200).send("Balance updated")
-        } catch (error) {
-          res.status(409).send("Balance is not valid")
-        }
-    } else (
-        res.status(409).send("Balance error")
-    )
-};
+    const { balance } = req.body
+    const validationBalance = Joi.object({
+        balance: Joi.number().min(1),
+    })
+    // if (balance >= 1) {
+    try {
+        await User.findByIdAndUpdate(req.user._id, { ...req.body, balance: validationBalance }, { new: true })
+        res.status(200).send("Balance updated")
+    } catch (error) {
+        res.status(409).send("Balance is not valid")
+        // }
+        // } else (
+        //     res.status(409).send("Balance error")
+        // )
+    }
+}
