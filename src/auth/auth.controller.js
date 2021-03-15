9a1983bdc20a40babf3f7f003699c186103e37ec
Natali Baziuchenko <baziuchenko_natali@ukr.net>
2021-03-15 11:36:16 +0200

    corrections

--- src/auth/auth.controller.js
+++ src/auth/auth.controller.js
@@ -1,25 +1,27 @@
-const fs = require("fs");
+const fs = require('fs');
 const { unlink } = fs.promises;
 
-const bcrypt = require("bcryptjs");
-const { v4: uuidv4 } = require("uuid");
-const Avatar = require("avatar-builder");
-const minifyImage = require("imagemin");
-const imageminPngquant = require("imagemin-pngquant");
-const cloudinary = require("cloudinary").v2;
-const sgMail = require("@sendgrid/mail");
-const jwt = require("jsonwebtoken");
-const Joi = require("joi");
+const bcrypt = require('bcryptjs');
+const { v4: uuidv4 } = require('uuid');
+const Avatar = require('avatar-builder');
+const minifyImage = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
+const cloudinary = require('cloudinary').v2;
+const sgMail = require('@sendgrid/mail');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
 
 const { HttpCodes } = require('../helpers/constants');
-const User = require('../user/User'); 
+const User = require('../user/User');
 
 async function logoutUser(req, res) {
   const { _id } = req.user;
   const userById = await User.findByIdAndUpdate(_id, { token: null });
 
   if (!userById) {
-    return res.status(401).send("Not authorized");
+    return res
+      .status(HttpCodes.NOT_AUTORIZED)
+      .json({ message: 'Not autorized' });
   }
 
   return res.status(204);
@@ -35,7 +37,7 @@ async function registerUser(req, res) {
     email: body.email,
   });
   if (isEmailExist) {
-    return res.status(409).send({ message: "Email in use" });
+    return res.status(409).send({ message: 'Email in use' });
   }
   const avatarTitle = Date.now();
   const avatar = Avatar.builder(
@@ -47,10 +49,10 @@ async function registerUser(req, res) {
     },
   )
     .create(body)
-    .then((buffer) => fs.writeFileSync(`tmp/${avatarTitle}.png`, buffer));
+    .then(buffer => fs.writeFileSync(`tmp/${avatarTitle}.png`, buffer));
 
   const files = await minifyImage([`tmp/${avatarTitle}.png`], {
-    destination: "public/images",
+    destination: 'public/images',
     plugins: [
       imageminPngquant({
         quality: [0.6, 0.8],
@@ -76,9 +78,9 @@ async function registerUser(req, res) {
     password: hashedPassword,
     verificationToken: tokenToVerify,
   });
-  
+
   if (!user) {
-    return res.status(500).send({ message: "Something went wrong" });
+    return res.status(500).send({ message: 'Something went wrong' });
   }
 
   // await sendVerificationEmail(body.email, tokenToVerify);
@@ -86,7 +88,7 @@ async function registerUser(req, res) {
     id: user.id,
     email: user.email,
     name: user.name,
-  }
+  };
   res.status(201).json({
     registrationResp,
   });
@@ -112,24 +114,31 @@ async function loginUser(req, res) {
   if (!user) {
     return res
       .status(HttpCodes.NOT_AUTORIZED)
-      .json({ message: "Not autorized" });
+      .json({ message: 'Not autorized' });
   }
 
   const isPasswordValid = await bcrypt.compare(password, user.password);
   if (!isPasswordValid) {
     return res
       .status(HttpCodes.NOT_AUTORIZED)
-      .json({ message: "Not autorized" });
+      .json({ message: 'Not autorized' });
   }
 
-  const token = jwt.sign({
-    userID: user._id,
-  }, process.env.JWT_SECRET);
+  const token = jwt.sign(
+    {
+      userID: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
 
-  const userNew = await User.findOneAndUpdate({ email }, { $set: { token } }, {
-    new: true
-  })
-  return res.status(HttpCodes.CREATED).json(userNew)
+  const userNew = await User.findOneAndUpdate(
+    { email },
+    { $set: { token } },
+    {
+      new: true,
+    },
+  );
+  return res.status(HttpCodes.CREATED).json(userNew);
 }
 
 async function validationUser(req, res, next) {
@@ -150,14 +159,14 @@ async function validationUser(req, res, next) {
 }
 
 async function authorization(req, res, next) {
-  const header = req.get("Authorization");
+  const header = req.get('Authorization');
   if (!header) {
     return res
       .status(HttpCodes.NOT_AUTORIZED)
-      .json({ message: "Not autorized" });
+      .json({ message: 'Not autorized' });
   }
 
-  const token = header.replace("Bearer ", "");
+  const token = header.replace('Bearer ', '');
 
   const payload = jwt.verify(token, process.env.JWT_SECRET);
   const { userID } = payload;
@@ -166,7 +175,7 @@ async function authorization(req, res, next) {
   if (!user) {
     return res
       .status(HttpCodes.NOT_AUTORIZED)
-      .json({ message: "Not authorized" });
+      .json({ message: 'Not authorized' });
   }
 
   req.user = user;
